45ffa60dec5c2bce23724503434ab1bf07446acf
raisircl <rai.verma@live.com>
2025-07-19 10:08:04 +0530

    json fetch

---
+++ main.js
@@ -0,0 +1,5 @@
+// Import specific functions
+import {add,subtract} from './mathUtils.js';
+
+console.log(add(5, 3));        // Output: 8
+console.log(subtract(9, 2));   // Output: 7
---
+++ mathUtils.js
@@ -0,0 +1,4 @@
+// Named exports
+export const add = (a, b) => a + b;
+export const subtract = (a, b) => a - b;
+export const multiply = (a, b) => a * b;
